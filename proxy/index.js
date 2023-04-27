85fa91204a99cc2bf087be29d0d5e235f5fa3e1d
Lucca Giusti <luccagiusti@gmail.com>
2023-04-27 17:28:45 +0200

    Added Patch support to the Proxy. (Attention, only for empty payload)

--- proxy/index.js
+++ proxy/index.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   const reqUrl = url.parse(req.url);
   var options = {
-    hostname: 'orion',
+    hostname: '192.168.1.100',
     port: 1026, // ORION Port
     path: reqUrl.path,
-    method: 'GET',
+    method: req.method,
     headers: {
       'Origin': '*',
       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Methods': 'GET, PATCH',
       'Access-Control-Allow-Origin': '*',
       'fiware-service': 'openiot',
       'fiware-servicepath': '/',
@@ -20,16 +20,29 @@ const server = http.createServer((req, res) => {
     }
   };
 
-  const proxy = http.request(options, (proxyRes) => {
-    var headers = proxyRes.headers;
-    headers['access-control-allow-headers'] = "*";
-    headers['access-control-allow-origin'] = "*";
-    headers['access-control-allow-methods'] = "GET";
-    res.writeHead(proxyRes.statusCode, headers);
-    proxyRes.pipe(res, { end: true });
-  });
-  
-  req.pipe(proxy, { end: true });
+  if (req.method === 'PATCH') {
+    const jsonData = JSON.stringify({ "type": "Property",  "value": "" } );
+    options.headers['Content-Length'] = jsonData.length;
+    options.headers['Content-Type'] = "application/json";
+    const proxyReq = http.request(options, (proxyRes) => {
+        proxyRes.pipe(res);
+    });
+    proxyReq.write(jsonData);
+    proxyReq.end();
+  } else {
+    const proxy = http.request(options, (proxyRes) => {
+
+        var headers = proxyRes.headers;
+        headers['access-control-allow-headers'] = "*";
+        headers['access-control-allow-origin'] = "*";
+        headers['access-control-allow-methods'] = "GET, PATCH";
+        res.writeHead(proxyRes.statusCode, headers);
+
+        proxyRes.pipe(res, { end: true });
+      });
+      
+      req.pipe(proxy, { end: true });
+  }
 });
 
 server.listen(port, () => {
